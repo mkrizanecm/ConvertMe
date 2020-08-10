33c0ced649d73b2159302626dc1d94051159f628
Matej Križanec <matej.krizanec97@gmail.com>
2020-08-10 21:02:48 +0200

    converter

--- main.php
+++ main.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php include 'converter.php'; ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script type="text/javascript" src="functions.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -10,24 +10,26 @@
 </head>
 <body>
 <div class="container">
-<h1>Convert.me</h1>
-<form method="post">
-    <div class="form-group">
-        <label for="exampleFormControlSelect1">Convert to: </label>
+    <div class="row justify-content-center align-self-center">
+        <h1>Convert.me</h1>
+    </div>
+    <form method="post" action="converter.php">
+        <div class="custom-file">
+            <input type="file" class="custom-file-input" id="customFile">
+            <label class="custom-file-label" for="customFile">Drop or browse file</label>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Convert to: </label>
             <select name="convert_options" class="form-control" id="exampleFormControlSelect1">
     
             </select>
-    </div>        
-    <div class="form-group">
-        <input name="convert" type="submit" class="btn btn-primary" value="Convert file" >
-    </div>
-
-    <div class="custom-file">
-        <input type="file" class="custom-file-input" id="customFile">
-        <label class="custom-file-label" for="customFile">Insert file</label>
-    </div>
-</form>
+        </div>        
+        <div class="form-group">
+            <input name="convert" type="submit" class="btn btn-primary" value="Convert file" >
+        </div>
+    </form>
 </div>
+<script type="text/javascript" src="js/functions.js"></script>
 </body>
 </html>
 
