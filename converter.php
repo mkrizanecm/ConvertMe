ba061165a762c0589bea94d8554ad4a8ea2f3eda
Matej Križanec <matej.krizanec97@gmail.com>
2020-08-15 11:36:13 +0200

    simplify

--- converter.php
+++ converter.php
@@ -1,10 +1,6 @@
 <?php 
 
-require_once('classes/Csv.php');
-require_once('classes/Json.php');
-require_once('classes/Xls.php');
-require_once('classes/Xlsx.php');
-require_once('classes/Xml.php');
+require_once('classes/Converter.php');
 
 if (!empty($_POST['convert'])) {
 
@@ -20,34 +16,33 @@ if (!empty($_POST['convert'])) {
     }
 
     $file_extension = pathinfo($file, PATHINFO_EXTENSION);
+
+    if ($file_extension == $convert_to) {
+        // Return same extension error
+    }
+
     $file_tmp_path = $_FILES['file_to_convert']['tmp_name'];
     $data_array = [];
     $final_file = '';
-    $class = '';
     switch ($file_extension) {
         case "json":
             $file_temp_data = file_get_contents($file_tmp_path);
             $data_array = json_decode($file_temp_data, true);
-            $class = 'Json';
             break;     
         case "xml":
             $data_array = simplexml_load_file($file_tmp_path);
-            $class = 'Xml';
             break;
         case "xls":
-            $class = 'Xls';
             break;  
         case "xlsx":
-            $class = 'Xlsx';
             break;
         case "csv":
-            $class = 'Csv';
             break;    
     }
 
-    $call = new $class();
-    $function_name = $file_extension . '_2_' . $convert_to;
-    $return_file = $call->$function_name($data_array);
+    $converter = new Converter();
+    $function_name = 'to_' . $convert_to;
+    $return_file = $converter->$function_name($data_array);
  
 }
 
